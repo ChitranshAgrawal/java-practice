845a6a17c5fa84e0a8314d73cfca1f8f4502b188
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-01-16 11:54:18 +0530

    for-each loop part 1 (for normal numbers)

--- Array.java
+++ Array.java
@@ -37,34 +37,48 @@ public class Array {
         // }
 
 
-        // array of objects
+        // // array of objects
 
-        Student s1 = new Student();
-        s1.rollNo = 001;
-        s1.name = "Gullu";
-        s1.marks = 99.2;
+        // Student s1 = new Student();
+        // s1.rollNo = 001;
+        // s1.name = "Gullu";
+        // s1.marks = 99.2;
 
-        Student s2 = new Student();
-        s2.rollNo = 002;
-        s2.name = "Chiku";
-        s2.marks = 98.5;
+        // Student s2 = new Student();
+        // s2.rollNo = 002;
+        // s2.name = "Chiku";
+        // s2.marks = 98.5;
 
-        Student s3 = new Student();
-        s3.rollNo = 003;
-        s3.name = "Chiya";
-        s3.marks = 92.23;
+        // Student s3 = new Student();
+        // s3.rollNo = 003;
+        // s3.name = "Chiya";
+        // s3.marks = 92.23;
 
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
+        // Student students[] = new Student[3];
+        // students[0] = s1;
+        // students[1] = s2;
+        // students[2] = s3;
 
-        // System.out.println(s1);
+        // // System.out.println(s1);
 
-        for ( int i = 0 ; i < students.length ; i++ ) {
-            System.out.println(students[i].rollNo + "  " + students[i].name + "  " + students[i].marks);
+        // for ( int i = 0 ; i < students.length ; i++ ) {
+        //     System.out.println(students[i].rollNo + "  " + students[i].name + "  " + students[i].marks);
+        // }
+
+
+        // enhanced for loop (for-each loop)
+
+        int nums[] = new int[50];
+
+        for ( int i : nums ) {
+            i = (int)(Math.random()*100);
+            System.out.print(i + "  ");
         }
 
     }
 }
+
+
+
+
 

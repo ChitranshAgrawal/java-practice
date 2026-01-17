577964826efa5d06dd7135e84ecf6403bb94b7e0
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-01-17 19:09:04 +0530

    Enum part 2

--- Enum.java
+++ Enum.java
@@ -4,8 +4,16 @@ enum Status {
 
 public class Enum {
     public static void main ( String args[] ) {
-        Status s = Status.Success;
-        System.out.println(s);
+        // Status s = Status.Success;
+        // System.out.println(s);
+
+        Status[] allStatus = Status.values();
+
+        for ( Status s : allStatus ) {
+            System.out.println(s + " at index: " + s.ordinal());
+        }
     }
 }
+
+
 

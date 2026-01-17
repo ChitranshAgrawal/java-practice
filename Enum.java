50aa965efdaf39d7375bbc965740c3d9247d6636
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-01-17 18:52:02 +0530

    Enum

---
+++ Enum.java
@@ -0,0 +1,11 @@
+enum Status {
+    Success, Failure, Pending, Unknown
+}
+
+public class Enum {
+    public static void main ( String args[] ) {
+        Status s = Status.Success;
+        System.out.println(s);
+    }
+}
+

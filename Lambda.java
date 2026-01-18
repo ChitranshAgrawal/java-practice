34f240f5d0a4962b55ce7bf270b1563f76f37862
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-01-18 13:01:42 +0530

    Anonymous inner class

---
+++ Lambda.java
@@ -0,0 +1,22 @@
+@FunctionalInterface
+interface A {
+    public void show();
+}
+
+public class Lambda {
+    public static void main ( String args[] ) {
+
+        // Anonymous inner class
+
+        A a = new A() {
+            public void show() {
+                System.out.println("in Show");
+            }
+        };
+        a.show();
+    }
+}
+
+
+
+
